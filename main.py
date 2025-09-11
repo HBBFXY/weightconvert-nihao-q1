41e0ce593212fc948792759e3fe1f331275cfe6e
nihao-q1 <3523139834@qq.com>
2025-09-11 18:15:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if WeightStr[-2]in("kg"):
     C=(eval(WeightStr[0:-2])/2.2046)
     print("转换后的重量是{:.3f}pb".format(C))
 elif WeightStr.endswith("pb"):
-    F=(eval(WightStr[0:-2])*2.2046)
+    F=(eval(WeightStr[0:-2])*2.2046)
     print("转换后的重量为{:.3f}kb".format(F))
 else:
     print("输入格式错误")
