5807fc16a96aa028bd0f1b4b943081813903c90e
nihao-q1 <3523139834@qq.com>
2025-09-11 18:13:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ WeightStr=input("请输入带有符号的重量值:")
 if WeightStr[-2]in("kg"):
     C=(eval(WeightStr[0:-2])/2.2046)
     print("转换后的重量是{:.3f}pb".format(C))
-elif RateStr.endswith("USB"):
+elif WeightStr.endswith("pb"):
     F=(eval(WightStr[0:-2])*2.2046)
     print("转换后的重量为{:.3f}kb".format(F))
 else:
