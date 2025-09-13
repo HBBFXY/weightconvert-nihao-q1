84e4d5301e308c9b9a0edf2064b646007b7a0e79
nihao-q1 <3523139834@qq.com>
2025-09-13 10:48:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #WeightConvert.py
 WeightStr=input()
-if WeightStr[-2]in("pd"):
-    C=(eval(WeightStr[0:-2])/2.2046)
-    print("转换后的重量是{:.3f}公斤".format(C))
+if WeightStr[-2]in("kg"):
+    C=(eval(WeightStr[0:-2])*2.2046)
+    print("转换后的重量是{:.3f}磅".format(C))
 elif WeightStr[-2]in("kg"):
-    F=(eval(WeightStr[0:-2])*2.2046)
-    print("转换后的重量为{:.3f}磅".format(F))
+    F=(eval(WeightStr[0:-2])/2.2046)
+    print("转换后的重量为{:.3f}公斤".format(F))
 else:
     print("输入格式错误")
