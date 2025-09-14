e0dd38ebbc0e7caf6b77b1ab545751cad902b944
nihao-q1 <3523139834@qq.com>
2025-09-14 21:54:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ WeightStr=input()
 if WeightStr[-2:]in("kg"):
     C=(eval(WeightStr[:-2])*2.2046)
     print("对应的英制重量为{:.3f}磅".format(C))
-elif WeightStr[-2:]in("pd"):
+elif WeightStr[-2:]in("磅"):
     F=(eval(WeightStr[:-2])/2.2046)
     print("对应的公制重量为{:.3f}公斤".format(F))
 else:
