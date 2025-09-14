6d7431a6f39b367595aac835bd4d79a226d2b842
nihao-q1 <3523139834@qq.com>
2025-09-14 20:45:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 WeightStr=input()
 if WeightStr[-2]in("kg"):
     C=(eval(WeightStr[0:-2])*2.2046)
-    print("转换后的重量是{:.3f}磅".format(C))
+    print("对应的英制重量为{:.3f}磅".format(C))
 elif WeightStr[-2]in("kg"):
     F=(eval(WeightStr[0:-2])/2.2046)
-    print("转换后的重量为{:.3f}公斤".format(F))
+    print("对应的公制重量为{:.3f}公斤".format(F))
 else:
     print("输入格式错误")
